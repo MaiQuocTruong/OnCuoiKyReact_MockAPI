b5c4fee8b5fcc812391041b2d60e3d38a9aff916
maiquoctruong <nguyenthanhtung0900@gmail.com>
2024-12-11 00:02:12 +0700

    react cuoi ki mockapi

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ScreenUser from './screens/ScreenUser';
+import ScreenDetailUser from './screens/ScreenDetailUser';
+import MedicineScreen from './screens/MedicineScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ title: 'Login' }} />
+        <Stack.Screen name="ScreenUser" component={ScreenUser} options={{ title: 'User List' }} />
+        <Stack.Screen name="ScreenDetailUser" component={ScreenDetailUser} options={{ title: 'User Detail' }} />
+        <Stack.Screen name="MedicineScreen" component={MedicineScreen} options={{ title: 'Home' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
